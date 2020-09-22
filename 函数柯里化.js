b10298f7558ade87168168e8785642e604e9c4e7
liualng <64637642+liualng@users.noreply.github.com>
2020-09-22 15:49:46 +0800

    Update 函数柯里化.js

--- 函数柯里化.js
+++ 函数柯里化.js
@@ -1,9 +1,17 @@
-// 函数柯里化是将一个多个参数的函数转化为一系列一个参数的函数
-function curry (fn) {
-  let args = Array.prototype.slice.call(arguments,1)
-  return function () {  
-    let innerArgs = Array.prototype.slice.call(arguments)
-    let finalArgs = args.concat(innerArgs);
-    return fn.apply(null,finalArgs);
-  }
+function createCurry(func) {
+  // 获取函数参数长度
+  const arity = func.length;
+  // 获取传进来的参数
+  const args = Array.prototype.slice.call(arguments, 1);
+  return function () {
+    // 合并参数
+    let _args = [].slice.call(arguments);
+    [].push.apply(_args, args);
+    // 参数没有传递完毕，继续递归
+    if (_args.length < arity) {
+      return createCurry(this, func, _args);
+    }
+    // 参数传递完毕，执行原始函数，并返回结果
+    return func.apply(this, _args);
+  };
 }
